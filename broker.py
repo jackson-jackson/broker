c4bc677f6d73929a82034feb8fb04f8d83b9903f
Jackson Warren <jacksonwarren86@gmail.com>
2017-10-25 12:36:08 -0700

    added option to store transaction details in database

--- broker.py
+++ broker.py
@@ -1,7 +1,36 @@
 import requests
+import sqlite3
+import datetime
+
+# create timestamp variable
+date = datetime.datetime.now()
+datestamp = str(date.strftime('%Y-%m-%d:%H.%M.%S'))
+
+
+def database():
+    conn = sqlite3.connect('broker.db')
+    c = conn.cursor()
+
+    def create_table():
+        c.execute('CREATE TABLE IF NOT EXISTS transactionDetails(datestamp, customer, currency, amount, fee, priceAfterFee, amountOfCrypto)')
+
+    def data_entry():
+        c.execute("INSERT INTO transactionDetails (datestamp, customer, currency, amount, fee, priceAfterFee, amountOfCrypto) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                  (datestamp, customer, currency, amount, fee, cad_price_after_fee, total_currency))
+        conn.commit()
+
+    create_table()
+    data_entry()
+    c.close()
+    conn.close()
+
 
 while True:
-    print('\nBuy ETH or BTC?')
+    # gather details of details of transaction
+    print('\nWho is the buyer?')
+    customer = input()
+
+    print('\nAre they buying ETH or BTC?')
     currency = input()
 
     # get live market data from coinmarketcap.com
@@ -31,12 +60,18 @@ while True:
     price_currency = 'Price per ' + currency + ' (in CAD) = ${:0.2f}'. format(cad_price_after_fee)
     print(price_currency)
     total_currency = float(amount) / float(cad_price_after_fee)
-    you_receive = 'You receive {:0.5f} '.format(total_currency) + currency + ' for ${}'.format(amount)
+    you_receive = 'You receive {:0.5f} '.format(
+        total_currency) + currency + ' for ${}'.format(amount)
     print(you_receive)
     print('\n::::::::::::::: YOUR QUOTE :::::::::::::::\n')
 
+    print('Save transaction details to database? y/n')
+    save_to_db = input()
+    if save_to_db == 'y':
+        database()
+
     # ask to repeat
-    print('Get new quote? yes/no')
+    print('\nGet new quote? y/n')
     answer = input()
-    if answer == 'no':
+    if answer == 'n':
         exit()
